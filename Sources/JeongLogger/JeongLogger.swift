d2d19cc39d2ff5e6e9aee4d3764c79d27b426ca3
유정주 <dbwjdwn9216@gmail.com>
2023-03-20 22:44:46 +0900

    change open to public

--- Sources/JeongLogger/JeongLogger.swift
+++ Sources/JeongLogger/JeongLogger.swift
@@ -1,12 +1,12 @@
 import OSLog
 
 open class JeongLogger {
-    open static func log<T>(_ object: T?, level: OSLogType = .default) {
+    public static func log<T>(_ object: T?, level: OSLogType = .default) {
         let message = "\(String(describing: object))"
         os_log(level, "%@", message)
     }
     
-    open static func error<T>(_ object: T?, level: OSLogType = .error) {
+    public static func error<T>(_ object: T?, level: OSLogType = .error) {
         let message = "\(String(describing: object))"
         os_log(level, "%@", message)
     }
