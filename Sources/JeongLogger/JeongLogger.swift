3402163ace32a4748962030cc0cfaae9ad6ddfbb
유정주 <dbwjdwn9216@gmail.com>
2023-03-20 22:33:33 +0900

    Add public in method

--- Sources/JeongLogger/JeongLogger.swift
+++ Sources/JeongLogger/JeongLogger.swift
@@ -1,12 +1,12 @@
 import OSLog
 
 open class JeongLogger {
-    static func log<T>(_ object: T?, level: OSLogType = .default) {
+    open static func log<T>(_ object: T?, level: OSLogType = .default) {
         let message = "\(String(describing: object))"
         os_log(level, "%@", message)
     }
     
-    static func error<T>(_ object: T?, level: OSLogType = .error) {
+    open static func error<T>(_ object: T?, level: OSLogType = .error) {
         let message = "\(String(describing: object))"
         os_log(level, "%@", message)
     }
