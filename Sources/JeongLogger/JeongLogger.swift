bfa1e2655f8194af0ac1c93564f06561d6960ffb
유정주 <dbwjdwn9216@gmail.com>
2023-03-20 22:51:39 +0900

    guard문 추가

--- Sources/JeongLogger/JeongLogger.swift
+++ Sources/JeongLogger/JeongLogger.swift
@@ -2,11 +2,21 @@ import OSLog
 
 open class JeongLogger {
     public static func log<T>(_ object: T?, level: OSLogType = .default) {
+        guard let object = object else {
+            os_log(level, "%@", "object is nil")
+            return
+        }
+        
         let message = "\(String(describing: object))"
         os_log(level, "%@", message)
     }
     
     public static func error<T>(_ object: T?, level: OSLogType = .error) {
+        guard let object = object else {
+            os_log(level, "%@", "object is nil")
+            return
+        }
+        
         let message = "\(String(describing: object))"
         os_log(level, "%@", message)
     }
