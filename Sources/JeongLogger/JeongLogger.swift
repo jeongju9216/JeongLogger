e3f10c3790825f60055eccf1e858235f55659732
유정주 <dbwjdwn9216@gmail.com>
2023-03-21 15:37:34 +0900

    Quick Help 추가

--- Sources/JeongLogger/JeongLogger.swift
+++ Sources/JeongLogger/JeongLogger.swift
@@ -1,6 +1,10 @@
 import OSLog
 
 open class JeongLogger {
+    ///로그 출력
+    ///- Parameters:
+    ///     - object : 출력하고 싶은 객체(e.g. String)
+    ///     - level : 로그 레벨(debug, info, default, error, fault)
     public static func log<T>(_ object: T?, level: OSLogType = .default) {
         guard let object = object else {
             os_log(level, "%@", "object is nil")
@@ -11,6 +15,10 @@ open class JeongLogger {
         os_log(level, "%@", message)
     }
     
+    ///에러 로그 출력
+    ///- Parameters:
+    ///     - object : 출력하고 싶은 에러 객체
+    ///     - level : 로그 레벨(debug, info, default, error, fault)
     public static func error<T>(_ object: T?, level: OSLogType = .error) {
         guard let object = object else {
             os_log(level, "%@", "object is nil")
